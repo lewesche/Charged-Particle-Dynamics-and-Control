fd220900938687453e8577d05d5c3995ed80d1b3
Leif Wesche <37157762+lewesche@users.noreply.github.com>
2018-04-06 15:57:17 -0700

    Added a relative velocity calculator to accuratley track dynamic desired positions, set up rendering for 60 fps

--- Particle_PID_Control_2D.m
+++ Particle_PID_Control_2D.m
@@ -8,27 +8,27 @@ clc
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Inputs  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dt=0.01;        %Set Time Step (seconds)
-run_time=25;     %Set Run Time (seconds)
+dt=0.0333/2;        %Set Time Step (seconds)
+run_time=20;         %Set Run Time (seconds)
 t=[0:dt:run_time];    
 
 m=[1];              %Particle Mass
-qi=[3];             %Charge (Positive Only)
-q_variance=0.5;       %Max Random Charge Variance
-xi=[1];             %Initial X Position
-yi=[1];             %Initial Y Position
-vxi=[9];            %Initial X Velocity
-vyi=[-6];            %Initial Y Velocity
+qi=[5];             %Charge (Positive Only)
+q_variance=0.5;     %Max Random Charge Variance
+xi=[0];             %Initial X Position
+yi=[0];             %Initial Y Position
+vxi=[0];            %Initial X Velocity
+vyi=[0];           %Initial Y Velocity
 
-q0_max=40;                %Total Distributed Charge of Borders
-res=12;             %Total Number of Discrete Border Points
-b=3;
+q0_max=20;          %Total Distributed Charge of Borders
+res=40;             %Total Number of Discrete Border Points
+b=6;
 
-x_desired=[-1];      %Desired X Position
+x_desired=[-1];     %Desired X Position
 y_desired=[2];      %Desired Y Position
-Kp=6;               %PID Proportional Gain
-Kd=1;               %PID Derivative Gain
-Ki=0.2;             %PID Integral Gain
+Kp=5;               %PID Proportional Gain
+Kd=1.5;               %PID Derivative Gain
+Ki=1;             %PID Integral Gain
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  Math  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -51,11 +51,18 @@ vx=vxi;
 vy=vyi;
 v=[vx; vy];
 E=[x-x_desired; y-y_desired];
+x_desired_prev=[0]; y_desired_prev=[0];         v_desired=[0,0];
 F=[];
 fig=figure;
-set(gcf, 'Position', [0, 0, 600, 600]);
+set(gcf, 'Position', [0, 0, 1920, 1080]);
 for i=t
     tc=round(i);
+    %Calculate Desired Position
+    if mod(i/dt, 2*60) == 0
+        x_desired=randi([-b+1, b-1]);
+        y_desired=randi([-b+1, b-1]);
+    end
+    
     %Apply Charge Variance
     if mod(i,0.25) == 0
         for j=[1:n]
@@ -66,21 +73,26 @@ for i=t
     %Calculate Error
     e=[x-x_desired; y-y_desired];
     E=[E, e];
+    %Calculate Velocity of Desired Position
+    v_desired(1)=diff([x_desired_prev, x_desired])/dt;
+    v_desired(2)=diff([y_desired_prev, y_desired])/dt; 
+    x_desired_prev=x_desired; y_desired_prev=y_desired; 
+    
     
     %Implement PID Control Law
     if i==0
         int=[0, 0];
     else
-    if i/dt <= 50
+    if i/dt <= 20
         int(1)=Ki*trapz([0:dt:i],E(1, 2:end));
         int(2)=Ki*trapz([0:dt:i],E(2, 2:end));
     else
-        int(1)=Ki*trapz([(i-50*dt):dt:i],E(1, end-50:end));
-        int(2)=Ki*trapz([(i-50*dt):dt:i],E(2, end-50:end));
+        int(1)=Ki*trapz([(i-20*dt):dt:i],E(1, end-20:end));
+        int(2)=Ki*trapz([(i-20*dt):dt:i],E(2, end-20:end));
     end
     end
-    pid(1)=Kp*e(1) + Kd*vx +int(1);
-    pid(2)=Kp*e(2) + Kd*vy +int(2);
+    pid(1)=Kp*e(1) + Kd*(vx-v_desired(1)) +int(1);
+    pid(2)=Kp*e(2) + Kd*(vy-v_desired(2)) +int(2);
         
     qr=pid(1)*(x-b)^2;  ql=-pid(1)*(x+b)^2; qt=pid(2)*(y-b)^2;  qb=-pid(2)*(y+b)^2;
     qr=ones(1, res)*qr; ql=ones(1, res)*ql; qt=ones(1, res)*qt; qb=ones(1, res)*qb;
@@ -115,11 +127,12 @@ for i=t
     title(['Particle Soup (2D)      ', '(X,Y)_{Desired}=(', num2str([x_desired, y_desired]), ')      E_{xy}=(', num2str([round(e(1),2), round(e(2),2)]), ')      Time Elapsed:' num2str(tc), 'Sec'])
     hold off
     pause(dt)
-%    F=[F, getframe(fig)];    
+   F=[F, getframe(fig)];    
 end
 
-% v=VideoWriter('Particles_Controlled_2D_1.avi','Uncompressed AVI');
-% open(v)
-% writeVideo(v,F)
-% close(v)
+v=VideoWriter('Particles_Controlled_2D_5.avi','Uncompressed AVI');
+v.FrameRate = 60;
+open(v)
+writeVideo(v,F)
+close(v)
 
